e6de37957b5ea15b0adbf74a1cb11b2ccca628f6
Mittzera <hugocavalcantesr@gmail.com>
2022-02-09 15:35:52 -0300

    Added the remaining files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-function App() {
+import narutoImg from './images/naruto.png'
+import styled from 'styled-components';
+import { Quotes } from './components/quotes/Quotes';
+import { getQuote } from './services/quoteServices';
+import { useState } from 'react';
+import jutsoSound from './sounds/jutso.mp3'
+
+const audio = new Audio(jutsoSound);
+
+export default function App() {
+  const [quoteState, setQuoteState] = useState({ quote:'ok', speaker: 'Speaker'});
+
+  const onUpdate = async () => {
+    const quote = await getQuote();
+    audio.play();
+    setQuoteState(quote);
+  }
   return (
-    <h1>Hello World</h1>
+    <Content>
+    <Quotes {...quoteState} onUpdate={onUpdate}/>
+   <NarutoImg src={narutoImg} alt="naruto"></NarutoImg>
+    </Content>
   );
 }
 
-export default App;
+const Content = styled.div`
+  height: 100vh;
+  padding: 0 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NarutoImg = styled.img`
+max-width: 50vw;
+align-self: flex-end;
+`;
